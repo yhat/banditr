92f47b4e966f9b639e3615a7f8d3f1211dfc7b7d
Greg Lamp <lamp.greg@gmail.com>
2017-03-15 11:04:57 -0700

    readme + docs

---
+++ R/banditr.R
@@ -0,0 +1,24 @@
+#' banditr: R Client for the Bandit API
+#'
+#' The banditr package provides functions for saving metadata, sending email, 
+#' spawning jobs, and streaming metrics.
+#' 
+#' @section Email functions:
+#' \itemize{
+#'   \item \code{\link{email}}
+#'   \item \code{\link{add_attachment}}
+#' }
+#' 
+#' @section Metadata functions:
+#' \itemize{
+#'   \item \code{\link{stream}}
+#' }
+#'
+#' @section Jobs functions:
+#' \itemize{
+#'   \item \code{\link{run}}
+#' }
+#'
+#' @docType package
+#' @name banditr
+NULL
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # banditr
 Bandit R client.
+
+## Installation
+
+```R
+> library(devtools)
+> devtools::install_github("yhat/banditr")
+> library(banditr)
+```
+
+## Usage
+```R
+> ?banditr
+> ?banditr::email
+> ?banditr::add_attachment
+> ?banditr::stream
+> ?banditr::metadata
+> ?banditr::run
+```
+
